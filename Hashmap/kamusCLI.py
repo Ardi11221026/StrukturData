be6794b2d4b9a44ce3cc2e2b637005a2cae8aa36
ArdiDwi8104 <ardiduisaputra@gmail.com>
2023-11-14 15:36:20 +0800

    update

--- Hashmap/kamusCLI.py
+++ Hashmap/kamusCLI.py
@@ -1,3 +1,4 @@
+import os
 import random
 
 from RedBlackTree import RedBlackTree
@@ -9,48 +10,67 @@ class KamusCLI:
     def terjemahkan_indonesia_ke_inggris(self):
         kata_indonesia = input("Masukkan kata dalam bahasa Indonesia: ")
         if kata_indonesia:
-            with open("KamusCLI.txt", "r") as file:
-                lines = file.readlines()
+            if os.path.isfile("KamusCLI.txt"):
+                with open("KamusCLI.txt", "r") as file:
+                    lines = file.readlines()
                 for line in lines:
                     kata, terjemahan = line.strip().split(":")
                     if kata.lower() == kata_indonesia.lower():
                         print(f"Terjemahan dari '{kata_indonesia}' ke bahasa Inggris: {terjemahan}")
                         self.gimmick()
                         return
-            print(f"Kata '{kata_indonesia}' tidak ditemukan dalam kamus.")
+                print(f"Kata '{kata_indonesia}' tidak ditemukan dalam kamus.")
+            else:
+                print("Kamus belum dibuat.")
+            self.gimmick_kata_tidak_ditemukan()
 
     def terjemahkan_inggris_ke_indonesia(self):
         kata_inggris = input("Masukkan kata dalam bahasa Inggris: ")
         if kata_inggris:
-            with open("KamusCLI.txt", "r") as file:
-                lines = file.readlines()
+            if os.path.isfile("KamusCLI.txt"):
+                with open("KamusCLI.txt", "r") as file:
+                    lines = file.readlines()
                 for line in lines:
                     kata, terjemahan = line.strip().split(":")
                     if terjemahan.lower() == kata_inggris.lower():
                         print(f"Terjemahan dari '{kata_inggris}' ke bahasa Indonesia: {kata}")
                         self.gimmick()
                         return
-            print(f"Kata '{kata_inggris}' tidak ditemukan dalam kamus.")
+                print(f"Kata '{kata_inggris}' tidak ditemukan dalam kamus.")
+            else:
+                print("Kamus belum dibuat.")
+            self.gimmick_kata_tidak_ditemukan()
+
+    def gimmick_kata_tidak_ditemukan(self):
+        print("Apakah Anda ingin menambahkan kata ke dalam kamus?")
+        tambah_kata = input("Masukkan 'y' untuk menambahkan, atau 'n' untuk keluar: ")
+        if tambah_kata.lower() == 'y':
+            self.tambahkan_kata()
+        else:
+            print("Terima kasih!")
 
     def tambahkan_kata(self):
         kata = input("Masukkan kata dalam bahasa Indonesia: ")
         terjemahan = input("Masukkan terjemahan dalam bahasa Inggris: ")
         if kata and terjemahan:
             self.kamus.insert(kata.lower(), terjemahan.lower())
-            with open("kamusCLI.txt", "a") as file:
+            with open("KamusCLI.txt", "a") as file:
                 file.write(f"{kata.lower()}:{terjemahan.lower()}\n")
             print(f"Kata '{kata}' sudah ditambahkan ke dalam kamus.")
 
     def tampilkan_kata(self):
-        with open("KamusCLI.txt", "r") as file:
-            lines = file.readlines()
-            if not lines:
-                print("Kamus masih kosong.")
-            else:
-                print("Isi kamus:")
-                for line in lines:
-                    kata, terjemahan = line.strip().split(":")
-                    print(f"{kata}: {terjemahan}")
+        if not os.path.isfile("KamusCLI.txt"):
+            print("Kamus belum dibuat.")
+        else:
+            with open("KamusCLI.txt", "r") as file:
+                lines = file.readlines()
+                if not lines:
+                    print("Kamus masih kosong.")
+                else:
+                    print("Isi kamus:")
+                    for line in lines:
+                        kata, terjemahan = line.strip().split(":")
+                        print(f"{kata}: {terjemahan}")
 
     def gimmick(self):
         angka_acak = [random.randint(1, 100) for _ in range(10)]
@@ -61,9 +81,8 @@ class KamusCLI:
             print("\nMenu:")
             print("1. Terjemahkan ke Inggris")
             print("2. Terjemahkan ke Indonesia")
-            print("3. Tambahkan Kata")
-            print("4. Tampilkan Kamus")
-            print("5. Keluar")
+            print("3. Tampilkan Kamus")
+            print("4. Keluar")
 
             choice = input("Pilih menu (1-5): ")
 
@@ -72,10 +91,8 @@ class KamusCLI:
             elif choice == "2":
                 self.terjemahkan_inggris_ke_indonesia()
             elif choice == "3":
-                self.tambahkan_kata()
-            elif choice == "4":
                 self.tampilkan_kata()
-            elif choice == "5":
+            elif choice == "4":
                 break
             else:
                 print("Pilihan tidak valid. Silakan pilih antara 1-5.")
--- Hashmap/kamusGUI.py
+++ Hashmap/kamusGUI.py
@@ -20,9 +20,6 @@ class KamusGUI:
         self.button2 = tk.Button(master, text="Terjemahkan ke Indonesia", command=self.terjemahkan_inggris_ke_indonesia)
         self.button2.pack()
 
-        self.button3 = tk.Button(master, text="Tambahkan Kata", command=self.tambahkan_kata)
-        self.button3.pack()
-
         self.button4 = tk.Button(master, text="Tampilkan Kamus", command=self.tampilkan_kata)
         self.button4.pack()
 
@@ -33,51 +30,65 @@ class KamusGUI:
         kata_indonesia = self.show_input_dialog("Masukkan kata dalam bahasa Indonesia:")
         if kata_indonesia:
             # Membuka file KamusGUI.txt
-            with open("KamusGUI.txt", "r") as file:
-                lines = file.readlines()
-                for line in lines:
-                    kata, terjemahan = line.strip().split(":")
-                    if kata.lower() == kata_indonesia.lower():
-                        self.show_message(f"Terjemahan dari '{kata_indonesia}' ke bahasa Inggris: {terjemahan}")
-                        self.gimmick()
-                        return
-            self.show_message(f"Kata '{kata_indonesia}' tidak ditemukan dalam kamus.")
+            try:
+                with open("KamusGUI.txt", "r") as file:
+                    lines = file.readlines()
+                    for line in lines:
+                        kata, terjemahan = line.strip().split(":")
+                        if kata.lower() == kata_indonesia.lower():
+                            self.show_message(f"Terjemahan dari '{kata_indonesia}' ke bahasa Inggris: {terjemahan}")
+                            self.gimmick()
+                            return
+            except FileNotFoundError:
+                self.gimmick_kata_tidak_ditemukan(kata_indonesia)
 
     def terjemahkan_inggris_ke_indonesia(self):
         kata_inggris = self.show_input_dialog("Masukkan kata dalam bahasa Inggris:")
         if kata_inggris:
             # Membuka file KamusGUI.txt
-            with open("KamusGUI.txt", "r") as file:
-                lines = file.readlines()
-                for line in lines:
-                    kata, terjemahan = line.strip().split(":")
-                    if terjemahan.lower() == kata_inggris.lower():
-                        self.show_message(f"Terjemahan dari '{kata_inggris}' ke bahasa Indonesia: {kata}")
-                        self.gimmick()
-                        return
-            self.show_message(f"Kata '{kata_inggris}' tidak ditemukan dalam kamus.")
+            try:
+                with open("KamusGUI.txt", "r") as file:
+                    lines = file.readlines()
+                    for line in lines:
+                        kata, terjemahan = line.strip().split(":")
+                        if terjemahan.lower() == kata_inggris.lower():
+                            self.show_message(f"Terjemahan dari '{kata_inggris}' ke bahasa Indonesia: {kata}")
+                            self.gimmick()
+                            return
+            except FileNotFoundError:
+                self.gimmick_kata_tidak_ditemukan(kata_inggris)
+
+    def gimmick_kata_tidak_ditemukan(self, kata):
+        result = messagebox.askquestion("Konfirmasi", f"Kata '{kata}' tidak ditemukan dalam kamus. Apakah Anda ingin menambahkan kata dalam kamus?")
+        if result == "yes":
+            self.tambahkan_kata()
+        else:
+            self.show_main_menu()
 
     def tambahkan_kata(self):
         kata = self.show_input_dialog("Masukkan kata dalam bahasa Indonesia:")
         terjemahan = self.show_input_dialog("Masukkan terjemahan dalam bahasa Inggris:")
         if kata and terjemahan:
             self.kamus.insert(kata.lower(), terjemahan.lower())  # Convert to lowercase for case-insensitive matching
-            with open("kamusGUI.txt", "a") as file:
+            with open("KamusGUI.txt", "a") as file:
                 file.write(f"{kata.lower()}:{terjemahan.lower()}\n")  # Convert to lowercase for consistent storage
             self.show_message(f"Kata '{kata}' sudah ditambahkan ke dalam kamus.")
 
     def tampilkan_kata(self):
         # Membuka file KamusGUI.txt
-        with open("KamusGUI.txt", "r") as file:
-            lines = file.readlines()
-            if not lines:
-                self.show_message("Kamus masih kosong.")
-            else:
-                message = "Isi kamus:\n"
-                for line in lines:
-                    kata, terjemahan = line.strip().split(":")
-                    message += f"{kata}: {terjemahan}\n"
-                self.show_message(message)
+        try:
+            with open("KamusGUI.txt", "r") as file:
+                lines = file.readlines()
+                if not lines:
+                    self.show_message("Kamus masih kosong.")
+                else:
+                    message = "Isi kamus:\n"
+                    for line in lines:
+                        kata, terjemahan = line.strip().split(":")
+                        message += f"{kata}: {terjemahan}\n"
+                    self.show_message(message)
+        except FileNotFoundError:
+            self.show_message("Kamus belum dibuat.")
 
     def gimmick(self):
         angka_acak = [random.randint(1, 100) for _ in range(10)]
@@ -94,8 +105,13 @@ class KamusGUI:
         if result == "yes":
             self.master.destroy()
 
+    def show_main_menu(self):
+        self.master.deiconify()
 
 if __name__ == "__main__":
     root = tk.Tk()
+    lebar = 400
+    tinggi = 300
+    root.geometry(f"{lebar}x{tinggi}")
     app = KamusGUI(root)
     root.mainloop()
